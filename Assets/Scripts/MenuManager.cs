8906ab1fde9d909926c4df7b9833d682a44bc2d6
David Geisert <catduogames@gmail.com>
2014-01-29 21:19:27 -0800

    Updates to the API, networking compartmentalized, comments added, image support

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -22,7 +22,7 @@ public class MenuManager : MonoBehaviour, IJoviosPlayerListener, IJoviosControll
 		JoviosControllerStyle controllerStyle = new JoviosControllerStyle();
 		controllerStyle.AddAbsoluteJoystick("left", "woot!", "testButton2");
 		controllerStyle.AddButton2("right", new string[] {"Press the button1!"}, new string[] {"testButton1"});
-		controllerStyle.AddArbitraryButton(new int[] {-2, 6, 4, 4}, "new button", "response");
+		controllerStyle.AddArbitraryButton(new int[] {-2, 6, 4, 4}, "https://lh3.googleusercontent.com/tkd1HiDTviukc9Gs-z3kb7WtLLVthq9rlNYRxZ2psDupR5EvuwFEqnK1-37t5XnGfw61KQV2qrI", "response");
 		//controllerStyle.SetTextInput("What is your quest?", "Submit");
 		//controllerStyle.SetAccelerometerStyle(JoviosControllerAccelerometerStyle.Full);
 		jovios.SetControls(p.GetUserID(), controllerStyle);
